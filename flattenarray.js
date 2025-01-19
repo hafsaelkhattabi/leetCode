b146a17778c5cb400e88a35a1fee5d6217ea6ace
hafsaelkhattabi <hafssakhattabi35@gmail.com>
2025-01-19 14:41:42 +0100

    Create flattenarray.js

---
+++ flattenarray.js
@@ -0,0 +1,8 @@
+function flattenArray(arr) {  //an array in arr
+  //flat the array in arr
+    return arr.flat()
+}
+//input :
+let arr = [1,2,3,4,[5,6]]
+console.log(flattenArray(arr))
+//ouput: [ 1, 2, 3, 4, 5, 6 ]
