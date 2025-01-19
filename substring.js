931edcd2ddc49f23090fb29b7b7aa5b3af10d00c
hafsaelkhattabi <hafssakhattabi35@gmail.com>
2025-01-19 16:00:29 +0100

    Create substring.js

---
+++ substring.js
@@ -0,0 +1,9 @@
+function extractSubstring(str, start, end) {
+    return str.substring(start , end)
+}
+//input : 
+let str = "hello"
+let start = 1
+let end = 3
+console.log(extractSubstring(str, start, end))
+//output : lo
