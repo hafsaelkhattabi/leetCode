be123820828aa8b1c2a0d617b7bdba9b9fe0423b
hafsaelkhattabi <hafssakhattabi35@gmail.com>
2025-01-19 12:58:55 +0100

    Update remove fromarray.js

--- remove fromarray.js
+++ remove fromarray.js
@@ -1,6 +1,7 @@
 var removeElement = function(nums, val) {
-
+  // flter all val (input the user ) from the input array
+    // the filter method return an updated array with all modification in it !!!
     nums = nums.filter(num => num !== val);
-
+  // return the new array without val value
     return nums
 };
